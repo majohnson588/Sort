eb7e1712bbc0ed362a107f7a83cdea885b2fb123
majohnson588 <117084422+majohnson588@users.noreply.github.com>
2023-04-17 21:27:11 +0800

    Create Sort.h

---
+++ Sort.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+
+void InsertionSort(int* a, int n);
+void ShellSort(int* a, int n);
+void PrintArray(int* a, int n);
+void SelectionSort(int* a, int n);
+void Swap(int* p1, int* p2);
