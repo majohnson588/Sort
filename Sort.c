5bf735d12c1e7b6efc4d57b6d23b94fa4b45cb23
majohnson588 <117084422+majohnson588@users.noreply.github.com>
2023-04-18 23:06:13 +0800

    Update Sort.c

--- Sort.c
+++ Sort.c
@@ -2,9 +2,9 @@
 
 void Swap(int* p1, int* p2)
 {
-	int tmp = p1;
-	p1 = p2;
-	p2 = tmp;
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
 }
 
 void PrintArray(int* a, int n)
@@ -123,3 +123,221 @@ void SelectionSort(int* a, int n)
 		--end;
 	}
 }
+
+void AdjustUp(int* a, int child)
+{
+	int parent = (child - 1) / 2;
+	while (child > 0)
+	{
+		if (a[child] < a[parent])
+		{
+			Swap(&a[child], &a[parent]);
+			child = parent;
+			parent = (child - 1) / 2;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+//堆排序
+//升序建大堆，降序建小堆
+void HeapSort(int* a, int n)
+{
+	//建堆方式1：O(N*logN)
+	//for (int i = 0; i < n; ++i)
+	//{
+	//	AdjustUp(a, i);
+	//}
+
+	//建堆方式2：O(N)
+	for (int i = (n - 1 - 1) / 2; i >= 0; --i)
+	{
+		AdjustDown(a, n, i);
+	}
+
+	//O(N*logN)
+	int end = n - 1;
+	while (end > 0)
+	{
+		Swap(&a[0], &a[end]);
+		AdjustDown(a, end, 0);
+		--end;
+	}
+}
+
+//冒泡排序
+//时间复杂度：O(N^2)
+//最好O(N)
+//对比插入排序，谁最好？ - 还是冒泡排序好
+void BubbleSort(int* a, int n)
+{
+	assert(a);
+
+	int exchange = 0;
+	for (int i = 1; i < n; ++i)
+	{
+		if (a[i - 1] > a[i])
+		{
+			Swap(&a[i - 1], &a[i]);
+			exchange = 1;
+		}
+	}
+
+	if (exchange == 0)
+	{
+		break;
+	}
+}
+
+
+//快速排序 - 最强算法
+// 时间复杂度O(N*logN)
+// 最坏是O(N^2),且若数组已经有序或接近有序则处于最坏或较坏情况，容易出现栈溢出
+// 原因是该算法无法判断是否已经有序
+// 最理想为key取中
+
+int GetMidIndex(int* a, int begin, int end)
+{
+	int mid = (begin + end) / 2;
+	if (a[begin] < a[mid])
+	{
+		if (a[mid] < a[end])
+		{
+			return mid;
+		}
+		else if (a[begin] < a[end])
+		{
+			return end;
+		}
+		else
+		{
+			return begin;
+		}
+	}
+	else //(a[begin] > a[mid])
+	{
+		if (a[mid] > a[end])
+		{
+			return mid;
+		}
+		else if (a[begin] < a[end])
+		{
+			return begin;
+		}
+		else
+		{
+			return end;
+		}
+	}
+}
+
+//1、hoare版本（霍尔版本）————hoare是发明这个算法的人
+int PartSort1(int* a, int begin, int end)
+{
+	int left = begin, right = end;
+	int keyi = a[left];//排升序时，左边做key右边先走，右边做key左边先走
+	while (left < right)
+	{
+		//右边先走，找小
+		while (left < right && a[right] >= a[keyi])
+		{
+			--right;
+		}
+
+		//左边再走，找大
+		while (left < right && a[left] <= a[keyi])
+		{
+			++left;
+		}
+
+		Swap(&a[left], &a[right]);
+	}
+
+	Swap(&a[keyi], &a[left]);
+
+	return keyi;
+}
+
+//2、挖坑法 - 细微差别（理解更简单）
+int PartSort2(int* a, int begin, int end)
+{
+	int key = a[begin];
+	int piti = begin;
+	while (begin < end)
+	{
+		//右边找小，填到左边的坑里面去。这个位置形成新的坑
+		while (begin < end && a[end] >= key)
+		{
+			--end;
+		}
+
+		a[piti] = a[end];
+		piti = end;
+
+		//左边找大，填到右边的坑里面去。这个位置形成新的坑
+		while (begin < end && a[begin] <= key)
+		{
+			++begin;
+		}
+
+		a[piti] = a[begin];
+		piti = begin;
+	}
+
+	a[piti] = key;
+	return piti;
+}
+
+//3、前后指针法
+int PartSort3(int* a, int begin, int end)
+{
+	int prev = begin;
+	int cur = begin + 1;
+	int keyi = begin;
+
+	//三数取中
+	int midi = GetMidIndex(a, begin, end);
+	Swap(&a[keyi], &a[midi]);
+
+	while (cur <= end)
+	{
+		//cur位置的值小于keyi位置值
+		if (a[cur] < a[keyi] && ++prev != cur)
+			Swap(&a[prev], &a[cur]);
+
+		++cur;
+	}
+
+	Swap(&a[prev], &a[keyi]);
+	keyi = prev;
+
+	return keyi;
+}
+
+void QuickSort(int* a, int begin,int end)
+{
+	//区间不存在，或者只有一个值则不需要再处理
+	if (begin >= end)
+	{
+		return;
+	}
+
+	if (end - begin > 10)
+	{
+		//int keyi = PartSort1(a, begin, end);
+        //int keyi = PartSort2(a, begin, end);
+		int keyi = PartSort3(a, begin, end);
+		// [begin, keyi-1] keyi [keyi+1, end]
+		QuickSort(a, begin, keyi - 1);
+		QuickSort(a, keyi + 1, end);
+	}
+	else
+	{
+		InsertSort(a + begin, end - begin + 1);
+	}
+}
+
+
