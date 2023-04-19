24fc68eb579cfcb87fe040b170071163aee2ffea
majohnson588 <117084422+majohnson588@users.noreply.github.com>
2023-04-19 22:28:09 +0800

    Update Sort.h

--- Sort.h
+++ Sort.h
@@ -12,3 +12,11 @@ void SelectionSort(int* a, int n);
 void Swap(int* p1, int* p2);
 void BubbleSort(int* a, int n);
 void QuickSort(int* a, int begin,int end);
+void QuickSortNonR(int* a, int begin, int end);
+void MergeSort(int* a, int n);
+void _MergeSort(int* a, int begin, int end, int* tmp);
+void MergeSortNonR(int* a, int n);
+void CountSort(int* a, int n);
+
+//声明
+extern callCount;
